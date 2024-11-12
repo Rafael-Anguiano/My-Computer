5b59d0d36e8e0076bea8c37233e798a13d4486f3
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-11-12 11:51:58 -0600

    new Commit

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,67 +1,67 @@
-import { 
-    Html,
-    useGLTF,
-    Float,
-    PresentationControls, 
-    Text,
-    ContactShadows,
-    Environment
+import {
+  Html,
+  useGLTF,
+  Float,
+  PresentationControls,
+  Text,
+  ContactShadows,
+  Environment
 } from "@react-three/drei"
 
 const Experience = () => {
-    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
-    return <>
+  const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
+  return <>
 
-        <color attach={"background"} args={["#191919"]} />
-        <Environment preset={"city"} />
+    <color attach={"background"} args={["#191919"]} />
+    <Environment preset={"city"} />
 
-        <PresentationControls 
-            global
-            rotation={ [ 0.13, 0.1, 0 ] }
-            polar={ [ - 0.4, 0.2 ] }
-            azimuth={ [ - 1, 0.75 ] }
-            config={ { mass: 2, tension: 400 } }
-            snap={ { mass: 4, tension: 400 } }
-        >
-            <Float rotationIntensity={ 0.4 } >
-                <rectAreaLight 
-                    width={ 2.5 } 
-                    height={ 1.65 } 
-                    intensity={ 100 } 
-                    color={"#2f4678"} 
-                    rotation={ [ - 0.1, Math.PI, 0 ] }
-                    position={ [ 0, 0.55, - 1.15 ] }
-                />
-                <primitive object={ computer.scene } position-y={ - 1.2 }>
-                    <Html
-                        transform
-                        wrapperClass="htmlScreen"
-                        distanceFactor={ 1.17 }
-                        position={ [ 0, 1.56, - 1.4 ] }
-                        rotation-x={ - 0.256 }
-                    >
-                        <iframe src={"https://rafael-anguiano.vercel.app/"}/>
-                    </Html>
-                </primitive>
-                <Text
-                    font="./bangers-v20-latin-regular.woff"
-                    fontSize={ 1 }
-                    position={ [ 2, 0.75, 0.75 ] }
-                    rotation-y={ - 1.25 }
-                    maxWidth={ 2 }
-                >
-                    RAFAEL ANGUIANO
-                </Text>
-            </Float>
-        </PresentationControls>
-
-        <ContactShadows 
-            position-y={ -1.4 } 
-            opacity={ 0.4 } 
-            scale={ 5 } 
-            blur={ 2.4 } 
+    <PresentationControls
+      global
+      rotation={[0.13, 0.1, 0]}
+      polar={[- 0.4, 0.2]}
+      azimuth={[- 1, 0.75]}
+      config={{ mass: 2, tension: 400 }}
+      snap={{ mass: 4, tension: 400 }}
+    >
+      <Float rotationIntensity={0.4} >
+        <rectAreaLight
+          width={2.5}
+          height={1.65}
+          intensity={100}
+          color={"#2f4678"}
+          rotation={[- 0.1, Math.PI, 0]}
+          position={[0, 0.55, - 1.15]}
         />
-    </>
+        <primitive object={computer.scene} position-y={- 1.2}>
+          <Html
+            transform
+            wrapperClass="htmlScreen"
+            distanceFactor={1.17}
+            position={[0, 1.56, - 1.4]}
+            rotation-x={- 0.256}
+          >
+            <iframe src={"https://rafael-anguiano.vercel.app/"} />
+          </Html>
+        </primitive>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          position={[2, 0.75, 0.75]}
+          rotation-y={- 1.25}
+          maxWidth={2}
+        >
+          RAFAEL ANGUIANO
+        </Text>
+      </Float>
+    </PresentationControls>
+
+    <ContactShadows
+      position-y={-1.4}
+      opacity={0.4}
+      scale={5}
+      blur={2.4}
+    />
+  </>
 }
 
 export default Experience
